5088fb764801176ddd12fdf373ed5d47d86c28dd
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-09-19 12:06:22 -0500

    create login bases classes and feature

---
+++ untitled/src/test/java/stepsdefinitions/LoginStepDefinition.java
@@ -0,0 +1,49 @@
+package stepsdefinitions;
+
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.Cast;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+public class LoginStepDefinition {
+
+    @Managed
+    WebDriver hisBrowser;
+
+    @Before
+    public void setUp(){
+        OnStage.setTheStage(Cast.ofStandardActors());
+        OnStage.theActorCalled("user");
+        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(hisBrowser));
+    }
+
+
+    @Given("that the user is on the login page")
+    public void thatTheUserIsOnTheLoginPage() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("the user enter the following credentials")
+    public void theUserEnterTheFollowingCredentials(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+
+    }
+    @Then("the user should see the main page")
+    public void theUserShouldSeeTheMainPage() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+}
