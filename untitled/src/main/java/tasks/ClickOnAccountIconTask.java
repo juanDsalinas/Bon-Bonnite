58846159088f108632cbfd45e17858fea9d737be
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-09-19 12:46:10 -0500

    updated given logic

--- untitled/src/main/java/tasks/ClickOnAccountIconTask.java
+++ untitled/src/main/java/tasks/ClickOnAccountIconTask.java
@@ -1,16 +1,18 @@
 package tasks;
 
-import interactions.ClickOnTargetInteraction;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
 import userinterfaces.MenuComponent;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class ClickOnAccountIconTask implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(new ClickOnTargetInteraction(MenuComponent.ACCOUNT_ICON));
+        actor.attemptsTo(Click.on(MenuComponent.ACCOUNT_ICON));
     }
     public static ClickOnAccountIconTask clickOnAccountIcon(){
-        return new ClickOnAccountIconTask();
+        return instrumented(ClickOnAccountIconTask.class);
     }
 }
