4a1b09a38b17feba799453b5befa564b64bbc030
juanDsalinas <juandsalinasnacun@gmail.com>
2023-09-19 14:34:12 -0500

    nothing

--- untitled/src/main/java/userinterfaces/RegisterPage.java
+++ untitled/src/main/java/userinterfaces/RegisterPage.java
@@ -3,13 +3,13 @@ package userinterfaces;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class RegisterPage {
-    public static final Target LBL_USERNAME = Target.the("the label of username")
+    public static final Target TXT_USERNAME = Target.the("the label of username")
             .locatedBy("//input[@id='reg_username']");
 
-    public static final Target LBL_ADDRESS = Target.the("the label of address")
+    public static final Target TXT_ADDRESS = Target.the("the label of address")
             .locatedBy("//input[@id='reg_email']");
 
-    public static final Target LBL_PASSWORD = Target.the("THE CHECKBOX")
+    public static final Target TXT_PASSWORD = Target.the("THE CHECKBOX")
             .locatedBy("//input[@id='privacy_policy_reg']");
 
     public static final Target BTN_REGISTER = Target.the("the btn to register")
