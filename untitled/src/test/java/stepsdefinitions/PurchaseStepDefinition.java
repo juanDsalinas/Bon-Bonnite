0f0fe29655c692ad550427a8afaa27a8c57ae25f
Santiago Bulla Perez <santiagobulla1216@gmail.com>
2023-09-20 09:11:08 -0500

    prepared the puschase feature

--- untitled/src/test/java/stepsdefinitions/PurchaseStepDefinition.java
+++ untitled/src/test/java/stepsdefinitions/PurchaseStepDefinition.java
@@ -1,5 +1,27 @@
 package stepsdefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.Cast;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
 public class PurchaseStepDefinition {
 
+    @Managed
+    WebDriver hisBrowser;
+
+    @Before
+    public void setUp(){
+        OnStage.setTheStage(Cast.ofStandardActors());
+        OnStage.theActorCalled("user");
+        OnStage.theActorInTheSpotlight().can(BrowseTheWeb.with(hisBrowser));
+    }
+
+    @After
+    public void tearDown(){
+        hisBrowser.quit();
+    }
 }
